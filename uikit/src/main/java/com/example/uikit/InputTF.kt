4bb0d701c30434b7688922783abc82995e13676d
unknown <alex_nerob@mail.ru>
2026-03-26 12:00:04 +0500

    доработка tf

--- uikit/src/main/java/com/example/uikit/InputTF.kt
+++ uikit/src/main/java/com/example/uikit/InputTF.kt
@@ -1,6 +1,14 @@
 package com.example.uikit
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -9,8 +17,10 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.TextStyle
@@ -23,14 +33,16 @@ import androidx.compose.ui.unit.dp
 //Алексей
 @Preview
 @Composable
-fun prevInput(){
+fun prevInput() {
     val value = remember { mutableStateOf("") }
     InputTF(
         value = value.value,
         onValueChange = {
             value.value = it
         },
-        placeholder = "Full Name"
+        placeholder = "Full Name",
+        modifier = Modifier.padding(horizontal = 20.dp),
+        withTrailingIcon = true
     )
 }
 
@@ -45,55 +57,55 @@ fun InputTF(
     withTrailingIcon: Boolean = false
 ) {
     val visualTransformation = remember { mutableStateOf(true) }
-    TextField(
+    BasicTextField(
         value = value,
-        onValueChange = {
-            onValueChange(it)
-        },
+        onValueChange = onValueChange,
         modifier = modifier,
-        colors = TextFieldDefaults.colors(
-            focusedTextColor = Black,
-            unfocusedTextColor = Black,
-            focusedPlaceholderColor = Black,
-            unfocusedPlaceholderColor = Black,
-            focusedIndicatorColor = Color(0xFFFF6480),
-            unfocusedIndicatorColor = Color(0xFFFF6480),
-            focusedTrailingIconColor = Black,
-            unfocusedTrailingIconColor = Black,
-            focusedContainerColor = Color.Transparent,
-            unfocusedContainerColor = Color.Transparent
+        textStyle = TextStyle(
+            color = Black
         ),
-        visualTransformation = if(withTrailingIcon){
-            if(visualTransformation.value){
-                PasswordVisualTransformation()
-            } else{
-                VisualTransformation.None
-            }
-        } else{
+        visualTransformation = if (withTrailingIcon && visualTransformation.value) {
+            PasswordVisualTransformation()
+        } else {
             VisualTransformation.None
         },
-        trailingIcon = {
-            if(withTrailingIcon) {
-                IconButton(
-                    onClick = {
-                        visualTransformation.value = !visualTransformation.value
-                    }
+        decorationBox = { innerTextField ->
+            Column {
+                Row(
+                    modifier = Modifier,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Icon(
-                        painter = painterResource(R.drawable.invisible_icon),
-                        contentDescription = null,
-                        tint = Color.Unspecified
-                    )
+
+                    Box(modifier = Modifier.weight(1f)) {
+                        if (value.isEmpty()) {
+                            Text(
+                                text = placeholder,
+                                style = Theme.typography.caption2Regular,
+                                color = Black
+                            )
+                        }
+                        innerTextField()
+                    }
+
+                    if (withTrailingIcon) {
+                        IconButton(
+                            onClick = {
+                                visualTransformation.value = !visualTransformation.value
+                            }
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.invisible_icon),
+                                contentDescription = null,
+                                tint = Color.Unspecified
+                            )
+                        }
+                    }
                 }
+                Box(modifier = Modifier
+                    .height(1.dp)
+                    .fillMaxWidth()
+                    .background(color = Color(0xFFFA5075)))
             }
-        },
-        placeholder = {
-            Text(text = placeholder,
-                style = Theme.typography.caption2Regular,
-                color = Black,
-                modifier = Modifier
-                    .padding(start = 0.dp))
-        },
-        singleLine = true
+        }
     )
 }
